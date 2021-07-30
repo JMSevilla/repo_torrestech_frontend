a14298f999cac0ea4eccc58157204f759733126d
Michael Jhon Navidad <81199287+mikee-pixel@users.noreply.github.com>
2021-07-30 11:08:12 +0800

    commit 07/30/2021

---
+++ src/components/product-template.vue
@@ -0,0 +1,61 @@
+<template>
+    <div>
+        <b-container fluid="md" class="main_banner_container">
+            <b-row class="banner-container">
+                <b-col lg="6" md="12">
+                    <center>
+                        <img src="@/assets/services-computer-programming-v1.png" alt="man sitting with computer">
+                    </center>
+                </b-col>
+                <b-col align-self="center" lg="6" md="12">
+                    <h1>Product Title Here</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    <div>
+                        <b-button variant="danger" size="lg">Contact Us</b-button>
+                    </div>
+                </b-col>
+            </b-row>
+            <b-row class="three_section">
+                <b-col>
+                    <h3>OUR PARTNERS:</h3>
+                    <ul>
+                        <li>
+                            <div>
+                                <i class="far fa-building"></i>
+                                <a href="http://" target="_blank" rel="noopener noreferrer">Dummy Company 1</a>
+                            </div>
+                        </li>
+
+                        <li>
+                            <div>
+                                <img class="" src="" alt="">
+                                <a href="http://" target="_blank" rel="noopener noreferrer">Dummy Company 2</a>
+                            </div>
+                        </li>
+                    </ul>
+                </b-col>
+                <b-col>
+                    <h3>FOLLOW US:</h3>
+                    
+                </b-col>
+                <b-col>
+                    <h3>GENERAL INFORMATION:</h3>
+                </b-col>
+            </b-row>
+        </b-container>
+
+    </div>
+</template>
+
+<style scoped>
+    .banner-container{
+        /* background-color: #BFC9CA; */
+        margin: 2% 0;
+        width: 100%;
+        height: 350px;
+        
+    }
+    .three_section{
+        margin: 2% 0;
+    }
+</style>
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,14 +1,19 @@
 <template>
     <div>
         <navigation />
+        <div>
+            <productpage />
+        </div>
     </div>
 </template>
 
 <script>
 import navigation from "@/components/nav"
+import productpage from "@/components/product-template"
+import productpage2 from "@/components/product-template2"
 export default {
     components: {
-        navigation
+        navigation, productpage, productpage2
     }
 }
 </script>
