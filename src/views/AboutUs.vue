85f7439f7c9156aaf4be863d3446af8d95cb1d0e
bisaisgithub <benaremail@gmail.com>
2021-07-31 08:15:16 +0800

    About Us page, its route and images

---
+++ src/views/AboutUs.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <navigation />
+    <aboutus />
+    <footerTorres />
+  </div>
+</template>
+
+<script>
+import navigation from "@/components/navbar/nav";
+import aboutus from "@/components/AboutUs/AboutUs.vue";
+import footerTorres from "@/components/footer";
+export default {
+  components: {
+    navigation,
+    aboutus,
+    footerTorres,
+  },
+};
+</script>
