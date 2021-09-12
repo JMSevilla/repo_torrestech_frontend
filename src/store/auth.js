cc895b5a56c3631d79c4dff43939acaee6dbf5ec
JM <miggysvll@gmail.com>
2021-09-12 22:18:47 +0800

    env update

--- src/store/auth.js
+++ src/store/auth.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
-
 export default axios.create({
-    baseURL: 'https://localhost:44351',
+    baseURL: process.env.TTC_LOCAL_ENV,
     headers: {
         "Content-type" : "application/json",
         "authapp" : "Basic:793bb6c2-4807-4805-a092-0a91d5ff62d7"
